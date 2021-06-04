9bbe3ed2b80fd8dede6983206a0d471891b4e8b7
jiahao <wangjiahao@openresty.com>
2021-06-04 16:13:55 +0800

    updated 1.19.9.1rc

--- v2/lua/openresty_org/templates.lua
+++ v2/lua/openresty_org/templates.lua
@@ -424,16 +424,16 @@ template_map['news-cn.tt2'] = function (context)
     local output = {}
     local i = 0
 
-i = i + 1 output[i] = '\n<div class="news">\n  <div class="news-item"><a href="ann-1019003002.html">\n      <picture>\n        <source srcset="../images/logo.webp" type="image/webp"/><img class="news-img" src="../images/logo.png"/>\n      </picture></a>\n    <div class="news-description"><strong class="news-hint">'
+i = i + 1 output[i] = '\n<div class="news">\n  <div class="news-item"><a href="ann-1019009001rc1.html">\n      <picture>\n        <source srcset="../images/logo.webp" type="image/webp"/><img class="news-img" src="../images/logo.png"/>\n      </picture></a>\n    <div class="news-description"><strong class="news-hint">'
 -- line 7 "news-cn.tt2"
-i = i + 1 output[i] = stash_get(stash, {'c', 0, 'l', { 'New!' }})
-i = i + 1 output[i] = '&nbsp;</strong><a href="ann-1019003002.html">OpenResty 1.19.3.2</a><span>&nbsp;'
--- line 7 "news-cn.tt2"
-i = i + 1 output[i] = stash_get(stash, {'c', 0, 'l', { 'is now released!' }})
-i = i + 1 output[i] = '</span></div>\n  </div>\n  <div class="news-item"><a href="ann-1019009001rc1.html">\n      <picture>\n        <source srcset="../images/logo.webp" type="image/webp"/><img class="news-img" src="../images/logo.png"/>\n      </picture></a>\n    <div class="news-description"><strong class="news-hint">'
--- line 13 "news-cn.tt2"
 i = i + 1 output[i] = stash_get(stash, {'c', 0, 'l', { 'New!' }})
 i = i + 1 output[i] = '&nbsp;</strong><a href="ann-1019009001rc1.html">OpenResty 1.19.9.1rc1</a><span>&nbsp;'
+-- line 7 "news-cn.tt2"
+i = i + 1 output[i] = stash_get(stash, {'c', 0, 'l', { 'is now released!' }})
+i = i + 1 output[i] = '</span></div>\n  </div>\n  <div class="news-item"><a href="ann-1019003002.html">\n      <picture>\n        <source srcset="../images/logo.webp" type="image/webp"/><img class="news-img" src="../images/logo.png"/>\n      </picture></a>\n    <div class="news-description"><strong class="news-hint">'
+-- line 13 "news-cn.tt2"
+i = i + 1 output[i] = stash_get(stash, {'c', 0, 'l', { 'New!' }})
+i = i + 1 output[i] = '&nbsp;</strong><a href="ann-1019003002.html">OpenResty 1.19.3.2</a><span>&nbsp;'
 -- line 13 "news-cn.tt2"
 i = i + 1 output[i] = stash_get(stash, {'c', 0, 'l', { 'is now released!' }})
 i = i + 1 output[i] = '</span></div>\n  </div>\n  <div class="news-item"><a href="https://blog.openresty.com.cn/cn/edge-upload-ssl-certs?src=org_news" target="_blank"><img class="news-img" src="../images/header-images/ssl-cover.jpg" loading="lazy"/></a>\n    <div class="news-description"><strong class="news-hint">'
@@ -477,16 +477,16 @@ template_map['news-en.tt2'] = function (context)
     local output = {}
     local i = 0
 
-i = i + 1 output[i] = '\n<div class="news">\n  <div class="news-item"><a href="ann-1019003002.html">\n      <picture>\n        <source srcset="../images/logo.webp" type="image/webp"/><img class="news-img" src="../images/logo.png"/>\n      </picture></a>\n    <div class="news-description"><strong class="news-hint">'
+i = i + 1 output[i] = '\n<div class="news">\n  <div class="news-item"><a href="ann-1019009001rc1.html">\n      <picture>\n        <source srcset="../images/logo.webp" type="image/webp"/><img class="news-img" src="../images/logo.png"/>\n      </picture></a>\n    <div class="news-description"><strong class="news-hint">'
 -- line 7 "news-en.tt2"
-i = i + 1 output[i] = stash_get(stash, {'c', 0, 'l', { 'New!' }})
-i = i + 1 output[i] = '&nbsp;</strong><a href="ann-1019003002.html">OpenResty 1.19.3.2</a><span>&nbsp;'
--- line 7 "news-en.tt2"
-i = i + 1 output[i] = stash_get(stash, {'c', 0, 'l', { 'is now released!' }})
-i = i + 1 output[i] = '</span></div>\n  </div>\n  <div class="news-item"><a href="ann-1019009001rc1.html">\n      <picture>\n        <source srcset="../images/logo.webp" type="image/webp"/><img class="news-img" src="../images/logo.png"/>\n      </picture></a>\n    <div class="news-description"><strong class="news-hint">'
--- line 13 "news-en.tt2"
 i = i + 1 output[i] = stash_get(stash, {'c', 0, 'l', { 'New!' }})
 i = i + 1 output[i] = '&nbsp;</strong><a href="ann-1019009001rc1.html">OpenResty 1.19.9.1rc1</a><span>&nbsp;'
+-- line 7 "news-en.tt2"
+i = i + 1 output[i] = stash_get(stash, {'c', 0, 'l', { 'is now released!' }})
+i = i + 1 output[i] = '</span></div>\n  </div>\n  <div class="news-item"><a href="ann-1019003002.html">\n      <picture>\n        <source srcset="../images/logo.webp" type="image/webp"/><img class="news-img" src="../images/logo.png"/>\n      </picture></a>\n    <div class="news-description"><strong class="news-hint">'
+-- line 13 "news-en.tt2"
+i = i + 1 output[i] = stash_get(stash, {'c', 0, 'l', { 'New!' }})
+i = i + 1 output[i] = '&nbsp;</strong><a href="ann-1019003002.html">OpenResty 1.19.3.2</a><span>&nbsp;'
 -- line 13 "news-en.tt2"
 i = i + 1 output[i] = stash_get(stash, {'c', 0, 'l', { 'is now released!' }})
 i = i + 1 output[i] = '</span></div>\n  </div>\n  <div class="news-item"><a href="https://blog.openresty.com/en/edge-upload-ssl-certs?src=org_news" target="_blank"><img class="news-img" src="../images/header-images/ssl-cover.jpg" loading="lazy"/></a>\n    <div class="news-description"><strong class="news-hint">'
