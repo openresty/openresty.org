86aede1ff06c71799570c6cf343d4f5782bf0e24
bamboo <bamboo@openresty.com>
2022-03-11 19:21:42 +0800

    deps: upgrade fe deps and update util js script.

--- v2/util/fmtMd.js
+++ v2/util/fmtMd.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var marked = require('marked');
-var renderer = new marked.Renderer();
-var matchHtmlRegExp = /["'&<>]/;
+const fs = require('fs');
+const {marked} = require('marked');
+const renderer = new marked.Renderer();
+const matchHtmlRegExp = /["'&<>]/;
 
 function escapeHtml(string) {
-    var str = '' + string;
-    var match = matchHtmlRegExp.exec(str);
+    const str = '' + string;
+    const match = matchHtmlRegExp.exec(str);
 
     if (!match) {
         return str;
     }
 
-    var escape;
-    var html = '';
-    var index = 0;
-    var lastIndex = 0;
+    let escape;
+    let html = '';
+    let index = 0;
+    let lastIndex = 0;
 
     for (index = match.index; index < str.length; index++) {
         switch (str.charCodeAt(index)) {
@@ -60,7 +60,7 @@ marked.setOptions({
 renderer.link = function (href, title, text) {
     title = title || text;
     if (href === '#') {
-        var slugger = new marked.Slugger();
+        const slugger = new marked.Slugger();
         href = '#' + slugger.slug(text);
     }
     title = escapeHtml(title);
@@ -69,7 +69,7 @@ renderer.link = function (href, title, text) {
 
 // add link icon for each heading, just like github
 renderer.heading = function (text, level, raw, slugger) {
-    var anchorId = renderer.options.headerPrefix + slugger.slug(raw);
+    const anchorId = renderer.options.headerPrefix + slugger.slug(raw);
     return '<h' + level + '>'
         + '<a id="' + anchorId + '" class="header-anchor" href="#' + anchorId + '" title="copy permalink to clipboard">'
         + '<svg class="icon" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-link"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg>'
@@ -77,13 +77,13 @@ renderer.heading = function (text, level, raw, slugger) {
         + '</h' + level + '>\n';
 };
 
-var args = process.argv.slice(2);
+const args = process.argv.slice(2);
 if (args.length === 0) {
     console.log('please provide input file');
     process.exit(0);
 }
 
-var infile = args[0];
+const infile = args[0];
 fs.open(infile, 'r', function (err, fd) {
     if (err) {
         if (err.code === 'ENOENT') {
