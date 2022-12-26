231f9254597a1e0edbb68ce3ae77e60755d45fb5
liwang2019 <wangli@openresty.com>
2022-12-26 20:10:54 +0800

    change: removed the xray popup from the index and download page.

--- v2/js/main.js
+++ v2/js/main.js
@@ -48,43 +48,6 @@ $(document).ready(function() {
 	$('#blog-modal').on($.modal.CLOSE, function () {
 		$('#blog-iframe').attr('src', '');
 	});
-
-	const path = location.pathname;
-	const urlPrefix = path.startsWith('/en/') ? 'https://openresty.com/en' : 'https://openresty.com.cn/cn';
-	const isIndex = path.endsWith('/cn/') || path.endsWith('/en/');
-	const isDownload = path.endsWith('/download.html');
-	const src = isIndex ? 'org_index' : 'org_download';
-
-	const XRAY_MODAL_LOCAL = 'xray_modal';
-	let xrayModalLocal = localStorage.getItem(XRAY_MODAL_LOCAL);
-
-	if ((isIndex || isDownload) && Number(xrayModalLocal) !== 1 && Number(xrayModalLocal) !== -3) {
-		$('.xray-modal').removeClass('hide');
-	}
-
-	$('.xray-modal .btn-close').on('click', () => {
-		xrayModalLocal = xrayModalLocal === null ? -1 : Number(xrayModalLocal) - 1;
-		localStorage.setItem(XRAY_MODAL_LOCAL, xrayModalLocal);
-		$('.xray-modal').addClass('hide');
-  });
-
-	$('.xray-modal .btn-learn-more').on('click', () => {
-		localStorage.setItem(XRAY_MODAL_LOCAL, 1);
-		location.href = `${urlPrefix}/xray/?src=${src}`;
-  });
-
-	$('.form-xray-request-demo').on('submit', function(event) {
-		event.preventDefault();
-		localStorage.setItem(XRAY_MODAL_LOCAL, 1);
-		location.href = `${urlPrefix}/xray/request-demo/?src=${src}&${$(this).serialize()}`;
-  });
-
-	$('.form-xray-request-demo input').on('keydown', (event) => {
-		if (event.keyCode == 13) {
-			event.preventDefault();
-			$('.form-xray-request-demo').submit();
-		}
-  });
 });
 
 document.querySelectorAll('pre code:not([class])').forEach(function ($) {
