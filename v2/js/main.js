8e22bacab19d7c191e73710fc45417a27bba063b
liwang2019 <wangli@openresty.com>
2026-08-26 03:45:17 +0200

    change: restrict the blog modal iframe to OpenResty blog origins.

--- v2/js/main.js
+++ v2/js/main.js
@@ -38,11 +38,28 @@ $(document).ready(function() {
 	var blogModal = document.getElementById('blog-modal');
 	var blogIframe = document.getElementById('blog-iframe');
 
+	// Only allow the OpenResty blog origins to be embedded in the modal
+	// iframe; a compromised or hijacked RSS feed must not be able to load
+	// an arbitrary third-party page here (clickjacking / phishing).
+	var allowedIframeHosts = ['blog.openresty.com', 'blog.openresty.com.cn'];
+
+	function isAllowedIframeUrl(url) {
+		try {
+			var host = new URL(url, window.location.origin).hostname;
+			return allowedIframeHosts.indexOf(host) !== -1;
+		} catch (e) {
+			return false;
+		}
+	}
+
 	if (blogModal && blogIframe) {
 		$('.article-item').on('click', function (event) {
 			event.preventDefault();
-			blogIframe.src = $(this).attr('href');
-			blogModal.showModal();
+			var href = $(this).attr('href');
+			if (isAllowedIframeUrl(href)) {
+				blogIframe.src = href;
+				blogModal.showModal();
+			}
 		});
 
 		$('.blog-modal-close').on('click', function () {
